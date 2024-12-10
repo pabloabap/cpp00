e6a5c1c384f983213f7e4c40f692c0b42c587b0f
Pablo Abad Aparicio <pabad-ap@car1s3.42barcelona.com>
2024-12-10 18:55:06 +0100

    ex00 cpp styled

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,29 +1,38 @@
-//CABECERA
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pabad-ap <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/10 18:43:32 by pabad-ap          #+#    #+#             */
+/*   Updated: 2024/12/10 18:48:32 by pabad-ap         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include <iostream>
-#include <cctype>
+#include <string>
 
 int main (int argc, char **argv)
 {
-	int	arg_index	= 1;
-	int	char_index	= 0;
-	char	*sentence 	= NULL;
-	
+	std::string str;
+	int			arg_i;
+
 	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	else
 	{
-		while (arg_index < argc)
+		arg_i = 1;
+		while (arg_i < argc)
 		{
-			sentence = argv[arg_index];
-			while (*sentence != '\0')
+			str = argv[arg_i];
+			for ( std::string::iterator it=str.begin(); it!=str.end(); ++it)
 			{
-				if (isalpha(*sentence) && islower(*sentence))
-					*sentence = toupper(*sentence);
-				std::cout << *sentence;
-				*sentence ++; 
+				if (isalpha(*it) && islower(*it))
+					*it = toupper(*it);
+				std::cout <<  *it;
 			}
-			arg_index++;
+			arg_i ++;
 		}
 	}
 	std::cout << std::endl;
